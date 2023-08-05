e4c957ff1cf626e212b81f2c6cf591138cbfd96d
page <aragan@unca.edu>
2023-08-05 11:19:00 -0400

    added completion html

--- app.py
+++ app.py
@@ -13,7 +13,27 @@ responses = []
 def show_home():
     return render_template('home.html', survey=survey)
 
+@app.route('/begin', methods=["POST"])
+def begin_survey():
+    return redirect('/questions/0')
+
+@app.route('/answer')
+def handle_question():
+    choice = request.form['answer']
+
+    responses.append(choice)
+
+    if (len(responses) == len(survey.questions)):
+        return redirect('/complete')
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
 @app.route('/questions/<int:qid>')
 def show_question(qid):
     question = survey.questions[qid]
     return render_template('question.html', question_num=qid, question=question)
+
+
+@app.route('/complete')
+def complete():
+    return render_template('complete.html')
