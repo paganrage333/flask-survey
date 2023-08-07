4ed5b0638879d7106d6997130806050372feb718
page <aragan@unca.edu>
2023-08-07 12:06:45 -0400

    added session

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template, redirect, flash 
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
+
+RESPONSES_KEY = "responses"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -11,42 +13,49 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 
-@app.route('/')
+@app.route("/")
 def show_home():
-    return render_template('home.html', survey=survey)
+    return render_template("home.html", survey=survey)
 
-@app.route('/begin', methods=["POST"])
+@app.route("/begin", methods=["POST"])
 def begin_survey():
-    return redirect('/questions/0')
 
-@app.route('/answer')
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
+
+@app.route("/answer", methods=["POST"])
 def handle_question():
     choice = request.form['answer']
 
+    responses = session[RESPONSES_KEY]
     responses.append(choice)
+    session[RESPONSES_KEY] = responses
 
     if (len(responses) == len(survey.questions)):
-        return redirect('/complete')
+        return redirect("/complete")
     else:
         return redirect(f"/questions/{len(responses)}")
 
-@app.route('/questions/<int:qid>')
+@app.route("/questions/<int:qid>")
 def show_question(qid):
+
+    responses = session.get(RESPONSES_KEY) 
 
     if (responses is None):
         return redirect('/')
     
     if (len(responses) == len(survey.questions)):
-        return redirect('/complete')
+        return redirect("/complete")
     
     if (len(responses) != qid):
         flash(f"Invalid question id: {qid}.")
         return redirect(f"/questions/{len(responses)}")
 
     question = survey.questions[qid]
-    return render_template('question.html', question_num=qid, question=question)
+    return render_template("question.html", question_num=qid, question=question)
 
 
-@app.route('/complete')
+@app.route("/complete")
 def complete():
-    return render_template('complete.html')
+    return render_template("complete.html")
